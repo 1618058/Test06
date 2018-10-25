6a9de5918c0a582d0b5830a33bae97916f690a80
1618058 <1618058@cs16058>
2018-10-25 15:02:21 +0900

    test06

--- src/speechtotext/SpeechtoText_main.java
+++ src/speechtotext/SpeechtoText_main.java
@@ -2,7 +2,10 @@ package speechtotext;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.model.RecognizeOptions;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechRecognitionResults;
@@ -12,7 +15,7 @@ public class SpeechtoText_main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 SpeechToText service = new SpeechToText();
-		    service.setUsernameAndPassword("1618058", "1618058");
+		    service.setUsernameAndPassword("8e44b6e2-e113-44b6-8a59-8cf19f2e0bf2", "237hKKWAKnDq");
 
 		    File audio = new File("audio/output.wav");
 		    RecognizeOptions options = null;
@@ -29,6 +32,22 @@ public class SpeechtoText_main {
 		    SpeechRecognitionResults transcript = service.recognize(options).execute();
 
 		    System.out.println(transcript);
+		    String s = String.valueOf(transcript);
+		    ObjectMapper mapper = new ObjectMapper();
+		    
+		    try {
+				JsonNode node = mapper.readTree(s);
+				for (int i = 0; i < node.get("results").size(); i++) {
+				String text = node.get("results").get(0).get("alternatives").get(0).get("transcript").toString();
+				String confidence = node.get("results").get(0).get("alternatives").get(0).get("confidence").toString();
+				System.out.println("transcript" + text);
+				System.out.println("confidence" + confidence);
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
 	}
 
 }
